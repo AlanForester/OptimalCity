d18b72b4e2dadc98e5f0d3f0922fc8c680fd2cbb
Alan Forester <alan@forester.pro>
2025-08-17 14:38:08 +0300

    Fix workflow

--- weather_display/spec/spec_helper.rb
+++ weather_display/spec/spec_helper.rb
@@ -14,21 +14,24 @@ SimpleCov.start 'rails' do
   minimum_coverage 60
 end
 
-require 'spec_helper'
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
-
+# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
+# Checks for pending migrations and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove these lines.
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
-  config.fixture_path = Rails.root.join('spec/fixtures')
+  config.fixture_paths = [Rails.root.join('spec/fixtures')]
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
